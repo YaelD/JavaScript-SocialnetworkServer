feaf647f4b109d6aae49751578667b8402af6bba
Yael Davidov <yaeldv@mta.ac.il>
2021-12-20 22:52:11 +0200

    First day of work

--- Server.js
+++ Server.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const StatusCodes = require('http-status-codes').StatusCodes;
 const package = require('./package.json');
+const crypto = require('crypto');
+const fs = require("fs").promises;
 
 const app = express()
 let  port = 2718;
@@ -23,23 +25,45 @@ app.use(express.urlencoded( // to support URL-encoded bodies
 }));
 
 
+// User's table
+const g_users = [ {id:1, name: 'Root'} ];
+
+const user_details_file = "/user_details.json"
+//const posts
+
+const tokens = [];
+
+const Token = function(user_id, user_token){
+	this.id = user_id;
+	this.token = user_token;
+}
+
+
+//User constructor 
 const User = function(email, password, full_name){
 	this.email_address = email;
-	this.password = password;
 	this.name = full_name;
-	this.id = user_id++;
+	this.id = find_max_id();
 	this.status = "created";
 	let today = new Date();
 	let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 	let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 	this.creation_date = date+' '+time; 
+	this.salt = crypto.randomBytes(16).toString('hex');
+	this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
+}
+
+function find_max_id()
+{
+	let max_id = 0;
+	g_users.forEach(
+		item => { max_id = Math.max( max_id, item.id) }
+	)
+	return (max_id+1);
 }
 
 
 
-
-// User's table
-const g_users = [ {id:1, name: 'Root'} ];
 
 // API functions
 
@@ -107,29 +131,117 @@ function delete_user( req, res )
 	res.send(  JSON.stringify( {}) );   
 }
 
-function create_user( req, res )
+async function register( req, res )
 {
-	const name = req.body.name;
+	const full_name = req.body.name;
+	const email = req.body.email;
+	const password = req.body.password;
 
-	if ( !name)
-	{
-		res.status( StatusCodes.BAD_REQUEST );
-		res.send( "Missing name in request")
+	if ( !is_valid_request( full_name, email, password, res)){
 		return;
 	}
 
+	if(check_if_email_exist(email, res)){
+		return;
+	}
 
-	// Find max id 
-	let max_id = 0;
-	g_users.forEach(
-		item => { max_id = Math.max( max_id, item.id) }
-	)
+	const new_user = new User(email, password, full_name);
+	g_users.push( new_user );
 
-	const new_id = max_id + 1;
-	const new_user = { id: new_id , name: name};
-	g_users.push( new_user  );
-	
-	res.send(  JSON.stringify( new_user) );   
+	const token = crypto.randomBytes(7).toString("hex");
+	tokens.push(new Token(new_user.id, token));
+	res.setHeader("Token",token);
+
+	write_user_data_to_file(new_user);
+
+	res.send(JSON.stringify(new_user, ['name', 'email', 'id', 'status', 'creation_date']));    
+}
+
+async function write_user_data_to_file( new_user)
+{ 	
+	let path_dir = "./users/" + new_user.id
+	await fs.mkdir(path_dir, (err) => {
+		if (err) {
+			console.log(err);
+		}
+		console.log("Directory is created.");
+	});
+	//let path = new_user.id + user_details_file;
+	await fs.writeFile( path_dir + user_details_file, JSON.stringify(new_user) );
+}
+
+async function check_if_email_exist(email, res)
+{
+	let is_email_exist = false;
+	for( let i = 1; i < user_id; i++)
+	{
+		let json_data = await fs.readFile( './users/' + i + user_details_file );
+		let user_data =JSON.parse(json_data);
+		if(user_data.email == email)
+		{
+			is_email_exist = true;
+			res.status( StatusCodes.BAD_REQUEST );
+			res.send("Email is already exist");
+		}
+	}
+	return is_email_exist;
+}
+
+async function exists( path )
+{
+    try {
+        const stat = await fs.stat( path )
+        return true;
+    }
+    catch( e )
+    {
+        return false;
+    }    
+}
+
+async function get_num_of_files()
+{
+	if(!(await exists('./users'))){
+		user_id = 1;
+		await fs.mkdir('./users');
+	}
+
+	await fs.readdir( './users', (error, files) => { 
+		if(error){
+			user_id = 1;
+			fs.mkdir('./users');
+		}
+		else{
+			user_id = files.length; // return the number of files
+			console.log(totalFiles); // print the total number of files
+		}
+	 });
+	 
+}
+
+function is_valid_request ( full_name, email, password, res )
+{
+	let is_valid = true;
+	if ( !full_name )
+	{
+		res.status( StatusCodes.BAD_REQUEST );
+		res.send( "Missing name in request");
+		is_valid = false;
+	}
+	else if ( !email )
+	{
+		res.status( StatusCodes.BAD_REQUEST );
+		res.send( "Missing email in request");
+		is_valid = false;
+	}
+	else if( !password )
+	{
+		res.status( StatusCodes.BAD_REQUEST );
+		res.send( "Missing password in request");
+		is_valid = false;
+	}
+
+	return is_valid;
 }
 
 function update_user( req, res )
@@ -168,26 +280,33 @@ function update_user( req, res )
 
 // Routing
 
-
 const router = express.Router();
 
-//router.get('/version', (req, res) => { get_version(req, res )  } )
-router.get('/users', (req, res) => { list_users(req, res )  } )
-router.post('/users', (req, res) => { create_user(req, res )  } )
-router.put('/user/(:id)', (req, res) => { update_user(req, res )  } )
-router.get('/user/(:id)', (req, res) => { get_user(req, res )  })
-router.delete('/user/(:id)', (req, res) => { delete_user(req, res )  })
+// router.get('/version', (req, res) => { get_version(req, res )  } )
+// router.get('/users', (req, res) => { list_users(req, res )  } )
+// router.post('/users', (req, res) => { create_user(req, res )  } )
+// router.put('/user/(:id)', (req, res) => { update_user(req, res )  } )
+// router.get('/user/(:id)', (req, res) => { get_user(req, res )  })
+// router.delete('/user/(:id)', (req, res) => { delete_user(req, res )  })
 
-
-
+router.post('/users/login', (req, res) => { login(req, res) })
+router.post('/users/register', (req, res) => { register(req, res) })
+router.put('/users/(:id)/post', (req, res) => { posting_new_post(req, res) })
+router.delete('/users/(:id)/post/(:post_id)', (req, res) => { delete_a_post(req, res) })
+router.get('/users/(:id)/post', (req, res) => { get_all_user_posts(req, res) })
+router.put('/users/(:id)/message', (req, res) => { send_message(req, res) })
+router.get('/users/(:id)/message', (req, res) => { get_all_messages(req, res) })
+router.get('/admin/users', (req, res) => { get_all_users_by_admin(req, res) })
+router.put('/admin/users/(:id)', (req, res) => { update_user_status_by_admin(req, res) })
+router.post('/admin/message', (req, res) => { send_broadcast_message_by_admin(req, res) })
+router.delete('/admin/user/(:id)/posts/(:post_id)', (req, res) => { delete_a_post_by_admin(req, res) })
 
 app.use('/api',router)
 
 
 // Init 
 
+//init_server();
+get_num_of_files();
 let msg = `${package.description} listening at port ${port}`
 app.listen(port, () => { console.log( msg ) ; })
-
-
-
