501c31e6a821f71d7078ff380beec603c0cc9b9c
Matan <matanp103@gmail.com>
2022-02-02 14:09:58 +0200

    Server header fixes

--- Server.js
+++ Server.js
@@ -12,6 +12,9 @@ const  port = 2718;
 const set_content_type = function (req, res, next) 
 {
 	res.setHeader("Content-Type", "application/json; charset=utf-8");
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	//res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	res.setHeader("Access-Control-Allow-Headers" ,"Content-Type");
 	next()
 }
 
