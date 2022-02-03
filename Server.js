0f5ab6095b68f49b4a026e6175bdefd742d61cc8
Yael Davidov <yaeldv@mta.ac.il>
2022-02-03 15:40:24 +0200

    Interval fixes

--- Server.js
+++ Server.js
@@ -14,7 +14,7 @@ const set_content_type = function (req, res, next)
 	res.setHeader("Content-Type", "application/json; charset=utf-8");
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	//res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-	res.setHeader("Access-Control-Allow-Headers" ,"Content-Type");
+	res.setHeader("Access-Control-Allow-Headers" ,"Content-Type, Authorization");
 	next()
 }
 
